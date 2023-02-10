3ffbfa3f7872aba115a4c747fbcf00912e497a0e
Mama-Thomas <tmama20012001@gmail.com>
2023-02-10 00:44:37 -0800

    created and style the login and register page

--- client/my_nb/src/App.js
+++ client/my_nb/src/App.js
@@ -14,6 +14,7 @@ import Coaches from "./pages/Coaches";
 import Teams from "./pages/Teams";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import './style.scss';
 
 const Layout = () => {
   return (
